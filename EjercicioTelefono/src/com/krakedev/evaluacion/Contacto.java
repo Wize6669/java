c104bd64b6cad19947003a1fd3a479d454624402
Wize669 <william.zapata@epn.edu.ec>
2023-12-18 16:45:49 -0500

    Ejercicio Telefono

--- EjercicioTelefono/src/com/krakedev/evaluacion/Contacto.java
+++ EjercicioTelefono/src/com/krakedev/evaluacion/Contacto.java
@@ -1,8 +1,11 @@
 package com.krakedev.evaluacion;
 
+import java.util.ArrayList;
+
 public class Contacto {
 	private String cedula, nombre, apellido;
 	private Direccion direccion;
+	private ArrayList<Telefono> telefonos= new ArrayList<Telefono>();
 	
 	public Contacto(String cedula, String nombre, String apellido) {
 		this.cedula = cedula;
@@ -42,6 +45,14 @@ public class Contacto {
 		this.direccion = direccion;
 	}
 	
+	public ArrayList<Telefono> getTelefonos() {
+		return telefonos;
+	}
+
+	public void setTelefonos(ArrayList<Telefono> telefonos) {
+		this.telefonos = telefonos;
+	}
+
 	public void imprimirDetalle () {
 		System.out.println("Información del Contacto \n" + "Cédula: " + this.cedula + "\n" + "Nombre: " + this.nombre + "\n" + "Apellido: " + this.apellido + "\n" + 
 				"Dirección: \n" + "Calle Principal: " +this.direccion.getCallePrincipal() + "\n" + "Calle Secundaria: " + this.direccion.getCalleSecundaria() 
@@ -58,6 +69,23 @@ public class Contacto {
 		System.out.println("***" + this.nombre + " " + this.apellido + "***" +"\n" + 
 				"Dirección: " + this.direccion.getCallePrincipal() + " y " + this.direccion.getCalleSecundaria() 
 	);
+	}
+	
+	public void mostrarTelefonos () {
+		System.out.println("Teléfono con estado 'C':");
+		for (Telefono telefono : telefonos) {
+			telefono.imprimirDetalle();
+		}
+	}
+	
+	public void mostrarIncorrectos () {
+		int incorrectos = 0;
+		for (Telefono telefono : telefonos) {
+			if(telefono.getEstado().equals("E")) {
+				incorrectos = incorrectos + 1;
+			}
+		}
+		System.out.println("Incorrectos: " + incorrectos);
 	}
 	
 }
--- EjercicioTelefono/src/com/krakedev/test/TestContacto.java
+++ EjercicioTelefono/src/com/krakedev/test/TestContacto.java
@@ -1,7 +1,10 @@
 package com.krakedev.test;
 
+import java.util.ArrayList;
+
 import com.krakedev.evaluacion.Contacto;
 import com.krakedev.evaluacion.Direccion;
+import com.krakedev.evaluacion.Telefono;
 
 public class TestContacto {
 
@@ -18,5 +21,23 @@ public class TestContacto {
 		d1.setCallePrincipal("Cdl del ejercito");
 		d1.setCalleSecundaria("Pasaje 2B");
 		c1.imprimirDetalleAct();
+		
+		System.out.println("");
+		ArrayList<Telefono> telefonos= new ArrayList<Telefono>();
+		Telefono t1 = new Telefono("1234567890", "Movil");
+		telefonos.add(t1);
+		Telefono t2 = new Telefono("9876543210", "Movil");
+		telefonos.add(t2);
+		Telefono t3 = new Telefono("9876543", "Convencional");
+		telefonos.add(t3);
+		
+		c1.setTelefonos(telefonos);
+		
+		c1.mostrarTelefonos();
+		
+		System.out.println("");
+		
+		c1.mostrarIncorrectos();
+		
 	}
 }
